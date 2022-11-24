e26851c41e67c135148b569c59bfd0781838aee4
chaitu-102 <86546260+chaitu-102@users.noreply.github.com>
2022-11-24 14:40:15 +0530

    Add files via upload

---
+++ DuplicateString.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class DuplicateString {
+	public static void  main(String[]args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the string:");
+	String a=s.nextLine();
+	a.replace(" ", "");
+	char[]arr=a.toCharArray();
+	int count;
+	for(int i=0;i<a.length();i++) {
+		count=1;
+		for(int j=i+1;j<a.length();j++) {
+			if(arr[i]==arr[j]&& arr[i]!=' ') {
+				count++;
+				arr[j]='0';
+			}
+		}
+				if(count>1 && arr[i]!='0') {
+					System.out.println("Duplicates characters are:"+arr[i]);
+					
+			}
+			
+	}
+}
+}
---
+++ RemoveDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class RemoveDuplicates {
+		public static void  main(String[]args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String a=s.nextLine();
+		a.replace(" ", "");
+		char[]arr=a.toCharArray();
+		for(int i=0;i<a.length();i++) {
+			for(int j=i+1;j<a.length();j++) {
+				if(arr[i]==arr[j]&& arr[i]!=' ') {
+					arr[j]='0';
+				}
+			}
+				}
+				for(int i=0;i<a.length();i++) {
+					if(arr[i]!='0') {
+					System.out.print(arr[i]);
+				}
+				}
+	}
+}
